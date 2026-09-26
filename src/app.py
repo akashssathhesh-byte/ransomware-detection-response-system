43f92f74003504c9643649fc958c6eca894dce30
akashssathhesh-byte <akashssathhesh@gmail.com>
2026-09-26 21:06:17 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,19 +1,10 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
-def dashboard():
-    return render_template("index.html")
-
-@app.route("/api/status")
-def status():
-    return jsonify({
-        "system": "RDRS",
-        "status": "Online",
-        "threats": 0,
-        "alerts": 0
-    })
+def home():
+    return "Ransomware Detection & Response System"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run()
